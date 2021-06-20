b4d88dacba2150d42075b1af72267b6e84d5e1d1
Fabian Bitter <fabian@bitter.de>
2021-06-20 19:58:44 +0200

    Update

--- public/application/config/concrete.php
+++ public/application/config/concrete.php
@@ -4,5 +4,8 @@ return [
     'white_label' => [
         'logo' => REL_DIR_APPLICATION . "/images/logo.png",
         'name' => "Professional Shop"
+    ],
+    'cache' => [
+        'doctrine_dev_mode' => true,
     ]
 ];
--- public/application/config/install/packages/professional_shop/controller.php
+++ public/application/config/install/packages/professional_shop/controller.php
@@ -4,6 +4,8 @@ namespace Application\StartingPointPackage\ProfessionalShop;
 
 use Concrete\Core\Application\Application;
 use Concrete\Core\Backup\ContentImporter;
+use Concrete\Core\Database\DatabaseStructureManager;
+use Concrete\Core\Package\Event\PackageEntities;
 use Concrete\Core\Package\Package;
 use Concrete\Core\Package\PackageService;
 use Concrete\Core\Package\Routine\AttachModeInstallRoutine;
@@ -11,6 +13,7 @@ use Concrete\Core\Package\StartingPointInstallRoutine;
 use Concrete\Core\Package\StartingPointPackage;
 use Concrete\Core\Page\Page;
 use Concrete\Core\Validation\CSRF\Token;
+use Doctrine\ORM\EntityManagerInterface;
 
 class Controller extends StartingPointPackage
 {
@@ -24,6 +27,30 @@ class Controller extends StartingPointPackage
     public function getPackageDescription()
     {
         return t("Starting point package to install a professional shop.");
+    }
+
+    public function finish()
+    {
+
+        $app = \Concrete\Core\Support\Facade\Application::getFacadeApplication();
+        /** @var PackageService $packageService */
+        $packageService = $app->make(PackageService::class);
+        $pkgClass = Package::getClass("bitter_shop_system");
+
+        if ($pkgClass) {
+            $packageService->install($pkgClass, []);
+        }
+
+        parent::finish();
+
+        $pev = new PackageEntities();
+        $app->make('director')->dispatch('on_refresh_package_entities', $pev);
+        $entityManagers = array_merge([$app->make(EntityManagerInterface::class)], $pev->getEntityManagers());
+
+        foreach ($entityManagers as $em) {
+            $manager = new DatabaseStructureManager($em);
+            $manager->refreshEntities();
+        }
     }
 
     public function install_content()

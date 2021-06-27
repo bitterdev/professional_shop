6f3d7e9585ee62398d3a9c69ba8f13f6e9a328c4
Bitter Digital Solutions UG (haftungsbeschränkt) <fabian@bitter.de>
2021-06-27 13:50:58 +0200

    Update

--- public/application/config/install/packages/professional_shop/controller.php
+++ public/application/config/install/packages/professional_shop/controller.php
@@ -1,18 +1,28 @@
-<?php
+<?php /** @noinspection PhpUndefinedClassInspection */
 
 namespace Application\StartingPointPackage\ProfessionalShop;
 
-use Concrete\Core\Application\Application;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportCategoriesRoutine;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportCouponsRoutine;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportCustomersRoutine;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportOrdersRoutine;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportPdfEditorRoutine;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportProductsRoutine;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportShippingCostsRoutine;
+use Bitter\BitterShopSystem\Backup\ContentImporter\Importer\Routine\ImportTaxRatesRoutine;
+use Bitter\BitterTheme\Backup\ContentImporter\Importer\Routine\ImportMultilingualContentRoutine;
 use Concrete\Core\Backup\ContentImporter;
+use Concrete\Core\Backup\ContentImporter\Importer\Manager;
+use Concrete\Core\Config\Repository\Repository;
 use Concrete\Core\Database\DatabaseStructureManager;
 use Concrete\Core\Package\Event\PackageEntities;
 use Concrete\Core\Package\Package;
 use Concrete\Core\Package\PackageService;
-use Concrete\Core\Package\Routine\AttachModeInstallRoutine;
-use Concrete\Core\Package\StartingPointInstallRoutine;
 use Concrete\Core\Package\StartingPointPackage;
 use Concrete\Core\Page\Page;
+use Concrete\Core\Support\Facade\Application;
 use Concrete\Core\Validation\CSRF\Token;
+use Concrete\Package\BitterShopSystem\Controller as ShopSystemPackageController;
 use Doctrine\ORM\EntityManagerInterface;
 
 class Controller extends StartingPointPackage
@@ -31,17 +41,9 @@ class Controller extends StartingPointPackage
 
     public function finish()
     {
-
-        $app = \Concrete\Core\Support\Facade\Application::getFacadeApplication();
-        /** @var PackageService $packageService */
-        $packageService = $app->make(PackageService::class);
-        $pkgClass = Package::getClass("bitter_shop_system");
-
-        if ($pkgClass) {
-            $packageService->install($pkgClass, []);
-        }
-
         parent::finish();
+
+        $app = Application::getFacadeApplication();
 
         $pev = new PackageEntities();
         $app->make('director')->dispatch('on_refresh_package_entities', $pev);
@@ -60,11 +62,14 @@ class Controller extends StartingPointPackage
          * So we need to install this package manually.
          */
 
-        $app = \Concrete\Core\Support\Facade\Application::getFacadeApplication();
-        /** @var PackageService $packageService */
-        $packageService = $app->make(PackageService::class);
+        $app = Application::getFacadeApplication();
         /** @var Token $token */
         $token = $app->make(Token::class);
+        $app = Application::getFacadeApplication();
+        /** @var PackageService $packageService */
+        $packageService = $app->make(PackageService::class);
+        /** @var Repository $config */
+        $config = $app->make(Repository::class);
 
         /** @noinspection PhpDeprecationInspection */
         $pkgClass = Package::getClass("bitter_theme");
@@ -79,7 +84,60 @@ class Controller extends StartingPointPackage
         Page::getByPath("/account/avatar")->delete();
         Page::getByPath("/account/messages")->delete();
 
-        parent::install_content();
+        /** @noinspection PhpDeprecationInspection */
+        $pkgClass = Package::getClass("bitter_shop_system");
+
+        if ($pkgClass) {
+            $packageService->install($pkgClass, []);
+
+            $pkgEntity = $packageService->getByHandle("bitter_shop_system");
+            /** @var ShopSystemPackageController $pkg */
+            $pkg = $pkgEntity->getController();
+
+            // Install sample content
+            if (is_dir($pkg->getPackagePath() . '/content_files')) {
+                $contentImporter = new ContentImporter();
+                /** @noinspection PhpUnhandledExceptionInspection */
+                $contentImporter->importFiles($pkg->getPackagePath() . '/content_files', true);
+            }
+
+            /*
+             * need to add all additional import routines here because multiple packages try to extend
+             * the 'import/item/manager' singleton.
+             */
+
+            /** @var ImporterManager $importer */
+            /** @noinspection PhpDeprecationInspection */
+            $app->bindshared(
+                'import/item/manager',
+                function ($app) {
+                    /** @var \Concrete\Core\Application\Application $app */
+                    /** @var Manager $importer */
+                    $importer = $app->make(Manager::class);
+                    foreach ($app->make('config')->get('app.importer_routines') as $routine) {
+                        $importer->registerImporterRoutine($app->make($routine));
+                    }
+                    $importer->registerImporterRoutine($app->make(ImportMultilingualContentRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportTaxRatesRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportShippingCostsRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportCategoriesRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportProductsRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportCustomersRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportOrdersRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportCouponsRoutine::class));
+                    $importer->registerImporterRoutine($app->make(ImportPdfEditorRoutine::class));
+
+                    return $importer;
+                }
+            );
+
+            $contentImporter = new ContentImporter();
+            $contentImporter->importContentFile($pkg->getPackagePath() . "/content.xml");
+
+            // enable public registration
+            $config->save('concrete.user.registration.enabled', true);
+            $config->save('concrete.user.registration.type', 'enabled');
+        }
     }
 
 }
